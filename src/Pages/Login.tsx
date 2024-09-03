6dfb00e5e7c83ff4dbf8869fc76e4b2b92ca8fe2
danya-gharbieh <38864792+danya-gharbieh@users.noreply.github.com>
2024-09-03 14:56:35 +0200

    Login functionality

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import FormField from "../components/FormField";
 import Header from "../components/Header";
 import { Views } from "../Views";
@@ -10,30 +11,55 @@ interface LoginProps {
 
 export default function Login(props: LoginProps) {
   const { t } = useTranslation();
+  const [formValues, setFormValues] = useState({ username: "", password: "" });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormValues({ ...formValues, [e.target.id]: e.target.value });
+  };
 
   /*ADD FUNCTIONALITY HERE PLEASE */
   function handleSubmit() {
-    props.callback(Views.Admin);
+    fetch(
+      "http://localhost:1337/api/users?filters[username][$eq]=" +
+        formValues.username
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.length);
+        if (data.length === 0) {
+          props.callback(Views.Login);
+          return;
+        }
+        console.log(data);
+        props.callback(Views.Admin);
+      });
   }
 
   return (
     <>
       <Header />
       <form>
-        <FormField label={t("Username")} id={"username"} type={"text"}></FormField>
-        <FormField label={t("Password")} id={"password"} type={"password"}></FormField>
+        <FormField
+          label={t("Username")}
+          id={"username"}
+          type={"text"}
+          value={formValues.username}
+          onChange={handleChange}
+        ></FormField>
+        <FormField
+          label={t("Password")}
+          id={"password"}
+          type={"password"}
+          value={formValues.password}
+          onChange={handleChange}
+        ></FormField>
 
-        <button onClick={handleSubmit} type="submit" className="mx-6 my-4 px-2 text-2xl border border-slate-900 hover:underline ">
-          Login
-        </button>
         <button
-          onClick={() => {
-            props.callback(Views.Admin);
-          }}
+          onClick={() => handleSubmit()}
           type="submit"
           className="mx-6 my-4 px-2 text-2xl border border-slate-900 hover:underline "
         >
-          Register
+          Login
         </button>
       </form>
     </>
--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -1,21 +1,28 @@
-import { HTMLInputTypeAttribute } from "react";
+import { ChangeEventHandler, HTMLInputTypeAttribute } from "react";
 
 interface FormFieldProps {
   label: String;
   id: HTMLInputTypeAttribute;
   type: HTMLInputTypeAttribute;
+  value: HTMLInputTypeAttribute;
+  onChange: ChangeEventHandler;
 }
 
 export default function FormField(props: FormFieldProps) {
   return (
     <div className="p-5 w-full">
       <div>
-        <label htmlFor={props.id} className="block mb-2 text-xl font-medium text-gray-900">
+        <label
+          htmlFor={props.id}
+          className="block mb-2 text-xl font-medium text-gray-900"
+        >
           {props.label}*
         </label>
         <input
           type={props.type}
           id={props.id}
+          value={props.value}
+          onChange={props.onChange}
           className="
                   bg-gray-50 border border-slate-900 text-gray-900 text-sm 
                   focus:ring-4 ring-offset-2 
