91ff305fcb378694e0d428f92a9e1b2c7bdea919
MaxPunzMaxPunz <162301249+MaxPunzMaxPunz@users.noreply.github.com>
2024-12-11 13:23:32 +0100

    Added Username and Role to the Header, Added some more german Translations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import LanguageSwitch from "./LanguageSwitch";
+import HeaderUsername from "./HeaderUsername";
 
 export default function Header() {
   return (
     <>
       <header className="flex flex-wrap items-center justify-between select-none max-w-full w-full p-5" role="banner" aria-label="Page Header">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold font-lexend flex-grow xl:basis-10/12 basis-3/5" aria-label="IIS Exam Page Title">IIS Exam Page</h1>
-        <div className="absolute right-5 flex-shrink-0" role="navigation" aria-label="Language Switch">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold font-lexend flex-grow xl:basis-10/12 basis-3/5" aria-label="IIS Exam Page Title">
+          IIS Exam Page
+        </h1>
+        <div className="pr-5" role="navigation" aria-label="Language Switch">
+          <HeaderUsername />
+        </div>
+        <div className="right-5 flex-shrink-0" role="navigation" aria-label="Language Switch">
           <LanguageSwitch />
         </div>
       </header>
