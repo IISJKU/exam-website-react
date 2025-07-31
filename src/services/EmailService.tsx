99a61f4c16199469c33a1453352efeb081915413
MaxPunzMaxPunz <162301249+MaxPunzMaxPunz@users.noreply.github.com>
2025-07-31 09:01:28 +0200

    Disabled popup if email failed (for now)

--- src/services/EmailService.tsx
+++ src/services/EmailService.tsx
@@ -15,21 +15,23 @@ export const sendEmail = async (params: SendEmailParams): Promise<void> => {
   if (to == "") return;
 
   try {
-    const response = await fetch(config.strapiUrl +"/api/send-email", {
+    const response = await fetch(config.strapiUrl + "/api/send-email", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`, // Use the token passed as a parameter
+        Authorization: `Bearer ${token}`, // Use the token passed as a parameter
       },
       body: JSON.stringify({ to, subject, text, html }),
     });
 
     if (!response.ok) {
       const errorData = await response.json();
-      showToast({ message: `Failed to send email: ${errorData.message}.`, type: "error" });
-      throw new Error(errorData.message || "Failed to send email");
+      console.log(`Failed to send email. ${errorData.message}`);
+      //showToast({ message: `Failed to send email: ${errorData.message}.`, type: "error" });
+      //throw new Error(errorData.message || "Failed to send email");
     }
   } catch (error) {
-    showToast({ message: `Failed to send email: ${error}.`, type: "error" });
+    console.log(`Failed to send email.`);
+    //showToast({ message: `Failed to send email: ${error}.`, type: "error" });
   }
 };
